6d942ff1fa9bf5ebbb9ecf5c4628a943c54ba3a0
sariasss <158272810+sariasss@users.noreply.github.com>
2026-05-05 11:15:06 +0200

    primer commit

---
+++ src/main/java/com/optativaGS/deportesUGR/DeportesUgrApplication.java
@@ -0,0 +1,13 @@
+package com.optativaGS.deportesUGR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeportesUgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeportesUgrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/optativaGS/deportesUGR/DeportesUgrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optativaGS.deportesUGR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeportesUgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
