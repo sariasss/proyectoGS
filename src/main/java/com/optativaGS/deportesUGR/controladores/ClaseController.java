995e26abd3e2028fe56672096d9947d679eab5b4
sariasss <158272810+sariasss@users.noreply.github.com>
2026-05-14 21:02:18 +0200

    final

--- src/main/java/com/optativaGS/deportesUGR/controladores/ClaseController.java
+++ src/main/java/com/optativaGS/deportesUGR/controladores/ClaseController.java
@@ -3,15 +3,13 @@ package com.optativaGS.deportesUGR.controladores;
 import com.optativaGS.deportesUGR.modelos.Clase;
 import com.optativaGS.deportesUGR.modelos.ClaseTipo3;
 import com.optativaGS.deportesUGR.modelos.Especialidad;
+import com.optativaGS.deportesUGR.modelos.RolUsuario;
 import com.optativaGS.deportesUGR.servicios.ClaseService;
 import com.optativaGS.deportesUGR.servicios.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -51,14 +49,27 @@ public class ClaseController {
     @GetMapping("/newClaseTipo3")
     public String nuevaClaseTipo3Form(Model model) {
         model.addAttribute("clase3", new ClaseTipo3());
-        model.addAttribute("entrenadores", usuarioService.findEntrenadores());
         model.addAttribute("especialidades", Especialidad.values());
         return "formularioAltaClase3";
     }
 
-    //Recoge la información del save clase tipo 3 y la guarda en la base de datos
+    @PostMapping("/filtrarClaseTipo3")
+    public String filtrar(@ModelAttribute("clase3") ClaseTipo3 claseTipo3, Model model) {
+
+        model.addAttribute("clase3", claseTipo3);
+        model.addAttribute("especialidades", Especialidad.values());
+        model.addAttribute("entrenadores",
+                usuarioService.findByRolAndEspecialidad(
+                        RolUsuario.ENTRENADOR,
+                        claseTipo3.getEspecialidad()
+                )
+        );
+
+        return "formularioAltaClase3";
+    }
+
     @PostMapping("/saveClaseTipo3")
-    public String newClass3(@ModelAttribute("clase3") ClaseTipo3 claseTipo3){
+    public String save(@ModelAttribute("clase3") ClaseTipo3 claseTipo3) {
         claseService.save(claseTipo3);
         return "redirect:/admin";
     }
